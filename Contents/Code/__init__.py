b9a342f3f8d137723c904a9285739597f7f19183
daeks <daeks@gmx.de>
2018-02-26 17:40:49 +0100

    initial commit

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,32 @@
+import hashlib, os, re
+
+COLLECTION_FLAG = '.plexcollection'
+
+def Start():
+  pass
+  
+def ValidatePrefs():
+  pass
+
+class CollectionAgent(Agent.Movies):
+  name = 'Collection Agent (Movies)'
+  languages = [Locale.Language.NoLanguage]
+  primary_provider = False
+  contributes_to = ['com.plexapp.agents.none', 'com.plexapp.agents.imdb', 'com.plexapp.agents.themoviedb']
+
+  def search(self, results, media, lang, manual=False):
+    if media.primary_agent == 'com.plexapp.agents.none':
+      results.Append(MetadataSearchResult(id = media.id, score = 100))
+    else:
+      results.Append(MetadataSearchResult(id = media.primary_metadata.id, score = 100))
+
+  def update(self, metadata, media, lang):
+    part = media.items[0].parts[0]
+    path = os.path.dirname(part.file)
+    (root_file, ext) = os.path.splitext(os.path.basename(part.file))
+    
+    if os.path.isfile(os.path.join(path, COLLECTION_FLAG):
+      collection = os.path.basename(path)
+      metadata.collection.clear()
+      metadata.collection.add(collection)
+      Log('[COLLECTION] Collection set to %s for %s' % (collection, root_file))
---
+++ README.md
@@ -0,0 +1,3 @@
+# Collection-Agent.bundle
+
+This agent creates a collection with the name of the parent folder if .plexcollection file exists within the folder
